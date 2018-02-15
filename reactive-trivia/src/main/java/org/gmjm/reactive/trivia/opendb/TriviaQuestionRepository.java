4d3ce9349a5caf6cf09e4caf01dd78ffa26fac60
Jonathan Kupcho <jonathan.kupcho@kohls.com>
2018-02-15 09:49:57 -0600

    initial

--- reactive-trivia/src/main/java/org/gmjm/reactive/trivia/opendb/TriviaQuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package org.gmjm.reactive.trivia.opendb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TriviaQuestionRepository extends MongoRepository<TriviaQuestion, String> {
-}
--- reactive-trivia/src/main/java/org/gmjm/reactive/trivia/slack/EventType.java
+++
@@ -1,5 +0,0 @@
-package org.gmjm.reactive.trivia.slack;
-
-public enum EventType {
-    message
-}
